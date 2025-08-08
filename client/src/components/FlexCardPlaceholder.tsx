b6ce797e5262b8579daef0ded1c39978b331bde1
Theodore Sergesketter <teddysergesketter@Theodores-MacBook-Pro.local>
2025-08-08 12:38:00 -0400

    Prepare fullstack Node + React app for Heroku

--- client/src/components/FlexCardPlaceholder.tsx
+++
@@ -1,8 +0,0 @@
-export default function FlexCardPlaceholder() {
-  return (
-    <div style={{ border: '1px solid #ccc', padding: '1rem', borderRadius: '4px' }}>
-      <h2>FlexCard Embed</h2>
-      <p>This is where your Salesforce FlexCard will appear.</p>
-    </div>
-  );
-}
